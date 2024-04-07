f2789b457f03cba8ad32ff78aec2aac76bafd23c
LONZEE <128856310+LONZEE@users.noreply.github.com>
2024-04-07 11:58:37 -0500

    Added basic requirements

--- Develop/utils/generateMarkdown.js
+++ Develop/utils/generateMarkdown.js
@@ -1,4 +1,6 @@
 // TODO: Create a function that returns a license badge based on which license is passed in
+const fs = require('fs');
+const inquirer = require('inquirer');
 // If there is no license, return an empty string
 function renderLicenseBadge(license) {}
 
